80b6caedcb369891f45947b58cc0d1121f30c7e1
Keamlak Kebede <keamlakk@gmail.com>
2026-06-02 23:46:46 -0400

    added starter models: college and student | set up admin

--- geoiteris/core/models.py
+++ geoiteris/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class College(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
+    
+class Student(models.Model):
+    graduation_year = models.IntegerField()
+    college = models.ForeignKey(College, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.graduation_year} - {self.college.name}"
+    
