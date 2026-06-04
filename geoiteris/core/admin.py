12e345b36b6d2476f3f2ea1f56a5ca391064a7e4
Keamlak Kebede <keamlakk@gmail.com>
2026-06-03 22:18:26 -0400

    update student model and admin use

--- geoiteris/core/admin.py
+++ geoiteris/core/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import College, Student
 
-admin.site.register(College)
-admin.site.register(Student)
+@admin.register(College)
+class CollegeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'graduation_year', 'college', 'github')
+    list_filter = ('graduation_year', 'college')
+    search_fields = ('name', 'github')
--- geoiteris/core/models.py
+++ geoiteris/core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class College(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, null=True)
     location = models.CharField(max_length=100)
     latitude = models.FloatField()
     longitude = models.FloatField()
@@ -10,9 +10,12 @@ class College(models.Model):
         return self.name
     
 class Student(models.Model):
+    name = models.CharField(max_length=100, null=True)
     graduation_year = models.IntegerField()
+    contact = models.EmailField(blank=True, null=True)
+    github = models.URLField(blank=True, null=True)
     college = models.ForeignKey(College, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.graduation_year} - {self.college.name}"
+        return f"{self.name} ({self.graduation_year})"
     
